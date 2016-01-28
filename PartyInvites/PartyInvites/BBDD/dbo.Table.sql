33294c437ffd9c88e8d1cd9005c49b03a07b6c70
jesus salvador <deadlyfire17@hotmail.com>
2016-01-28 14:14:06 +0100

    Guarda informacion en BBDD y manda Email ( si metes bien la info

---
+++ PartyInvites/PartyInvites/BBDD/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Phone] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(MAX) NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL
+)
--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Helpers;
 
 namespace PartyInvites.Controllers
 {
@@ -16,7 +20,7 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View();  // Carga la pagina Index.cshtml
         }
-        
+
         [HttpGet] // Indicamos que este metodo se ejecutara cuando se realice una peticion a este controlador y a la vista RsvpForm usando el metodo Get
         public ViewResult RsvpForm()
         {
@@ -29,15 +33,47 @@ namespace PartyInvites.Controllers
             // Aqui deberia ir el metodo de mandar Email que tenemos escrito en la pagina Thanks.cshtml
             if (ModelState.IsValid) // Esta sentencia recoge el estado de todos los atributos con campo Required del modelo GuestResponse que, al bindear usando la View, ya saben si validan o no, y este metodo los comprueba siendo True en caso de que todas las validaciones sean correctas.
             {
+                enviarEmail(guest);
+                registrarDatos(guest);                
                 return View("Thanks", guest); // Cargamos la pagina Thanks ( si queremos cargar una pagina en concreto, le pasamos un String con el nombre sin extension al View ) y le damos el modelo que lo recibe como Object la View 
                                               // ( si nos fijamos en thanks indicamos con @model que el modelo que le pasamos es GuestResponse, para entendernos, es como si al entrar en la View, 
                                               // la propia View le hiciera un Cast para poder trabajar con el modelo GuestResponse mediante la clase Model sin tener que hacerselo nosotros )
-            }                                 
+            }
             else
             {
                 return View();
             }
-            
+        }
+
+        private void registrarDatos(GuestResponse guest)
+        {
+            String conectionString = ConfigurationManager.ConnectionStrings["miConexion"].ConnectionString;
+            SqlConnection con = new SqlConnection(conectionString);
+            con.Open();
+            string sql = "insert into dbo.Guest (Phone, Name, Email, Attend) values ('" + guest.Phone + "','" + guest.Name + "','" + guest.Email + "','" + guest.WillAttend + "');";
+
+            SqlCommand query = new SqlCommand(sql, con);
+            query.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void enviarEmail(GuestResponse guest)
+        {             
+            try
+            {
+                WebMail.SmtpServer = "smtp.example.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "MySmtpUserName";
+                WebMail.Password = "MySmtpPassword";
+                WebMail.From = "rsvps@example.com";
+                WebMail.Send("party-host@example.com", "RSVP Notification", guest.Name + " is " + ((guest.WillAttend ?? false) ? "" : "not") + " attending");
+                ViewBag.Email = "An Email was sent succesfully to our organizator.";
+            }
+            catch (Exception)
+            {
+            ViewBag.Email = "Sorry - we couldn't send the email to confirm your RSVP";
+            }            
         }
     }
 }
